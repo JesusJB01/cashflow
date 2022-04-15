e5a62445e145439587a93eb93411282113a6e790
JesusJB01 <95654994+JesusJB01@users.noreply.github.com>
2022-04-14 20:08:15 -0500

    primer commit
    primer commit

---
+++ src/components/Resume/Index.vue
@@ -0,0 +1,83 @@
+<template>
+    <main>
+        <p>{{ label }}</p>
+        <h1>{{ amountCurrency }}</h1>
+        <h1>{{ datevisual }}</h1>
+        <div class="graphic">
+            <slot name="graphic"></slot>
+        </div>
+        <div class="action">
+            <slot name="action"></slot>
+        </div>
+    </main>
+</template>
+
+<script>
+
+const currencyFormatter = new Intl.NumberFormat("es-MX", {
+    style: "currency",
+    currency:"MXN",
+}); 
+
+export default{
+    props: {
+        label: {
+            type: String,
+        },
+
+        datevisual:{
+            type: String,
+        }, 
+
+        date: {
+            type: String,
+        },
+        totalAmount: {
+            type: Number,
+        },
+        amount: {
+            type: Number,
+        },
+    },
+    computed:{
+        amountVisual(){
+            return this.amount !== null ? this.amount :this.totalAmount;
+        }, 
+        date(){
+            return this.date !== null ? this.date :this.datevisual;
+        },
+        amountCurrency(){
+            return currencyFormatter.format(this.amountVisual);
+        },
+    },
+
+};
+</script>
+
+
+<style scoped>
+main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+}
+h1,
+p {
+  margin: 0;
+  text-align: center;
+}
+h1 {
+  margin-top: 14px;
+  color: var(--brand-green);
+}
+.graphic {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  padding: 48px 24px;
+  box-sizing: border-box;
+}
+</style>
